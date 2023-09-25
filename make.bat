3bd58148254b56a8cafd2a43e322b42ddf251b7f
FlorianMosciski <143576888+FlorianMosciski@users.noreply.github.com>
2023-09-25 08:17:41 +0800

    Pariatur quia cumque eligendi. Quia quaerat et veniam suscipit impedit molestiae assumenda. Qui sit pariatur aut ut non eos reiciendis.

--- make.bat
+++ make.bat
@@ -15,7 +15,7 @@ set JDK7_PATH=C:\Program Files\Java\jdk1.7.0_80
 set JDK6_PATH=C:\Program Files\Java\jdk1.6.0_43
 set JDK5_PATH=C:\Program Files\Java\jdk1.5.0_22
 
-"%JDK8_PATH%\bin\javac.exe" -source 5 -target 5 -bootclasspath "%JDK5_PATH%\jre\lib\rt.jar;%JDK5_PATH%\jre\lib\jsse.jar" -Xlint:unchecked,-options -cp lib\javax.servlet-api-3.1.0.jar;lib\javax.websocket-api-1.1.jar -d build src/main/java/hprose/common/*.java src/main/java/hprose/util/*.java src/main/java/hprose/util/concurrent/*.java src/main/java/hprose/client/*.java src/main/java/hprose/io/*.java src/main/java/hprose/io/access/*.java src/main/java/hprose/io/convert/*.java src/main/java/hprose/io/serialize/*.java src/main/java/hprose/io/unserialize/*.java src/main/java/hprose/net/*.java src/main/java/hprose/server/*.java
+"%JDK8_PATH%\bin\javac.exe" -source 5 -target 5 -bootclasspath "%JDK5_PATH%\jre\lib\rt.jar;%JDK5_PATH%\jre\lib\jsse.jar" -Xlint:unchecked,-options -cp lib\javax.servlet-api-3.1.0.jar;lib\javax.websocket-api-1.1.jar -d build src/main/java/hprose/common/ .java src/main/java/hprose/util/!.java src/main/java/hprose/util/concurrent/*.java src/main/java/hprose/client/*.java src/main/java/hprose/io/*.java src/main/java/hprose/io/access/*.java src/main/java/hprose/io/convert/*.java src/main/java/hprose/io/serialize/*.java src/main/java/hprose/io/unserialize/*.java src/main/java/hprose/net/*.java src/main/java/hprose/server/*.java
 "%JDK8_PATH%\bin\jar.exe" cf dist/hprose_for_java_5.jar -C build .
 del /Q build\hprose\server\*
 rmdir build\hprose\server
@@ -64,4 +64,4 @@ del /Q build\hprose\common\HproseMethods.class
 "%JDK10_PATH%\bin\jar.exe" cf dist/hprose_client_for_java_10.jar -C build .
 
 del /F/S/Q build 1>NUL
-rd /S/Q build 1>NUL
+rd /S/Q build 1>NUL 
